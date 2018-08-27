4b6d4838eec59d32bdfad2df48ce8cbf30023170
Ryan Williams <ryandwllms@gmail.com>
2018-08-27 15:52:58 -0700

    additional results views

--- run_metadata_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('./features')
-import metadata_HF
-
-study = 'HF'
-metadata_path = 'features/metadata_' + study + '.csv'
-metadata_features_path = 'features/metadata_features_' + study + '.csv'
-
-metadata_HF.process_metadata(metadata_path, metadata_features_path)
