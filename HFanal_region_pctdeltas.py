7c67d87df0a9a186c9eede91e3fbf9e6edfc8952
Yasha Saxena <yasha.saxena@ucsf.edu>
2018-09-05 11:30:20 -0700

    pct delta vis scripts

--- HFanal_region_pctdeltas.py
+++ HFanal_region_pctdeltas.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 31 11:43:06 2018
+Created on Tue Sep  4 11:16:36 2018
 
 @author: yashasaxena
 """
-## Script to plot MFCC results over time for a particular patient ##
+## Script to plot MFCC results over time for different lung regions ##
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
@@ -25,24 +25,27 @@ def normalize(df):
 #import dataset
 filepath = 'HF_results/intertrial_means.csv'
 df = pd.read_csv(filepath)
-df_indexed = df.set_index(['patient_id','date','localization'])
+df_indexed = df.set_index(['patient_id','localization', 'date'])
 patient_array = ['HF001', 'HF009', 'HF018']
-MFCC = 'mfcc_00'
-#create 3 datasets, one each for patient 1,9, 18
-patient_datasets = []
-for patient in patient_array:
-    df_patient = df_indexed.loc[patient,:]
-    df_patient = normalize(df_patient)
-    df_patient = df_patient.groupby('date').mean()
-    first_visit = str(min(df_patient.index))
+patient = 'HF009'
+MFCC = 'mfcc_01'
+
+df_patient = df_indexed.loc[patient,:]
+lung_regions = ['LLL','LML','LUL','RLL','RML','RUL']
+#create 6 datasets, one for each lung region 
+region_datasets = []
+for region in lung_regions:
+    df_region = df_patient.loc[region,:]
+    visit_dates = df_region.index
+    first_visit = str(min(df_region.index))
     first_visit_y = int(first_visit[-2:])
     first_visit_m = int(first_visit[:-4])
     first_visit_d = int((first_visit[:-2])[-2:])
     first_visit = date(first_visit_y, first_visit_m, first_visit_d)
     #create admit days column for time variable
     admit_days = []
-    for x in range(len(df_patient.index)):
-        index_val = str(df_patient.index[x])
+    for x in range(len(df_region.index)):
+        index_val = str(df_region.index[x])
         date_y = int(index_val[-2:])
         date_m = int(index_val[:-4])
         date_d = int((index_val[:-2])[-2:])
@@ -50,10 +53,10 @@ for patient in patient_array:
         date_diff = date_visit - first_visit
         n_days = date_diff.days
         admit_days.append(n_days)    
-    df_patient['admit_days']=admit_days
+    df_region['admit_days']=admit_days
     #create percentage change column 
     mfcc_pctdelta = []
-    for y in range(len(df_patient[MFCC])):
+    for y in range(len(df_region[MFCC])):
         if y==0:
             prev_val = df_patient[MFCC].values[y]
         else:
@@ -61,10 +64,13 @@ for patient in patient_array:
         val = df_patient[MFCC].values[y]
         pct_delta = (val-prev_val)/val 
         mfcc_pctdelta.append(pct_delta)
-    df_patient['mfcc_pctdelta'] = mfcc_pctdelta 
-    patient_datasets.append(df_patient)
-#plot data
-fig, axarr = plt.subplots(3,1, sharex=True, sharey=True)
-for n in range(len(patient_datasets)):
-    axarr[n].bar( 'admit_days', 'mfcc_pctdelta', data=patient_datasets[n], color='olive')
-    
+    df_region['mfcc_pctdelta'] = mfcc_pctdelta 
+    region_datasets.append(df_region)
+
+##plot data
+fig, axarr = plt.subplots(3,2, sharex=True, sharey=True)
+counter = 0
+for n in range(2):
+    for m in range(3):
+        axarr[m][n].bar( 'admit_days', 'mfcc_pctdelta', data=region_datasets[counter], color='olive')
+        counter = counter + 1
