115d80453d6431f5daaf59e5605b5d8554ddebdc
Yasha Saxena <yasha.saxena@ucsf.edu>
2018-09-26 09:50:34 -0700

    added all possible patients to scatter plots in script, some misalignment with some of patient data will have to check data to determine what is going on

--- HFanal_fluid_v_feature_scatter.py
+++ HFanal_fluid_v_feature_scatter.py
@@ -7,7 +7,7 @@ Created on Tue Sep 11 13:11:45 2018
 """
 import matplotlib.pyplot as plt
 from HFanal_utils import *
-import os
+#import os
 # style selector for pyplot
 # print(plt.style.available)
 plt.style.use('seaborn-ticks')
@@ -17,24 +17,36 @@ features = get_localized_features()
 recordings = get_patient_recordings()
 
 """select patient id and location id for plots"""
-patient_id = ['HF001', 'HF009','HF018']
-location_id = ['LLL','RLL']
+    
+location_id = ['LLL']
 feature_list = ['mfcc_00','mfcc_01','mfcc_02','mfcc_03','mfcc_04', 'mfcc_05',
             'mfcc_06','mfcc_07','mfcc_08','mfcc_09','mfcc_10','mfcc_11']
+
 """plot fluid overload vs feature val for selected locations and all patients"""
 for feature in feature_list: 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    for patient in patient_id:
-        pat_features = normalize(features.loc[patient])
+
+    for patient in pat_idx:
+        #pat_features = normalize(features.loc[patient])
+        pat_features = features.loc[patient]
         fluids = get_fluid_list(patient)
         # swap level for location-based access, dates are sorted
         pat_features = pat_features.swaplevel(0, 1)
         locations = pat_features.index.levels[0].tolist()
         
         for location in location_id:
-            feature_vals = pat_features[feature].loc[location].tolist()
-            ax1.scatter(fluids,feature_vals, label = location)
-            plt.legend(loc='upper left')
+            try:
+                feature_vals = pat_features[feature].loc[location].tolist()
+                ax1.scatter(fluids,feature_vals, label = patient)
+                plt.title(feature)
+                plt.xlabel('Fluid Overload')
+                plt.ylabel('feature val')
+            except:
+                continue
+            
+    plt.title(feature)
+    plt.xlabel('Fluid Overload')
+    plt.ylabel('feature val')
     plt.show()
 
